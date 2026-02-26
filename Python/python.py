3f1a034c860ca89dcb057ef358c7edcec8b90bc6
Joshua-is-the-best-at-DTE <joshua.c.arada@outlook.com>
2026-02-27 12:00:34 +1300

    Learning the basics of python and databases. Small progress on footer

--- Python/python.py
+++ Python/python.py
@@ -1,10 +1,15 @@
 import sqlite3
 
-db = sqlite3.connect('products.db')
-cursor = db.cursor()
-sql = "SELECT * FROM ;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
+database = 'Databases/products.db'
 
-db.close
+def print_all_products():
+    with sqlite3.connect(database) as db:
+        cursor = db.cursor()
+        sql = "SELECT * FROM products;"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for Product_name in results:
+            print(f'Product name: {Product_name[0]} - Price: ${Product_name[4]}.00')
+
+if __name__ == "__main__":
+    print_all_products()
